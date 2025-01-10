76dff7a91b2fd34aee1c1dd1493a18a1d431b159
enduraew <enduraew@gmail.com>
2025-01-10 23:18:53 +0500

    upd

--- homework/hwork-001/script.js
+++ homework/hwork-001/script.js
@@ -1,7 +1,7 @@
-const myName = "Nikita";
+const myName = "Никита";
 const helloWorld = `Hello, ${myName}`;
 const timeLog = new Date().toLocaleString();
 
-const fullText = `Привет! Меня зовут ${myName}! Текущее время ${timeLog}`;
+const fullText = `Привет! Я открылся с дэлеем в 1000 мс! Меня зовут ${myName}! Твоё текущее время ${timeLog}`;
 
 setTimeout(() => alert(fullText), 1000);
