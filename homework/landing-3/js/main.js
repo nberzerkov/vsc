523329e95d004d8f40f38b35a071e19515ac4a22
enduraew <enduraew@gmail.com>
2025-01-28 22:32:54 +0500

    upd

--- homework/landing-3/js/main.js
+++ homework/landing-3/js/main.js
@@ -1,9 +1,9 @@
-(function() {
-    
+(function () {
+
     document.addEventListener('click', burgerInit)
 
     function burgerInit(e) {
-        
+
         const burgerIcon = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
 
@@ -14,6 +14,28 @@
             document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
+        }
+
+    }
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
         }
 
     }
